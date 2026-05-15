70c186f95fafc510abb2c4bb46caec894525344f
hys <helpyourselfes@gmail.com>
2026-05-15 17:43:48 +0300

    Add custom types for id's

--- app/models/models.go
+++ app/models/models.go
@@ -1,15 +1,18 @@
 package models
 
+type playlistID int64
+type trackID int64
+
 type Playlist struct {
-	ID            int64     `json:"id"`
-	Name          string    `json:"name"`
-	Path          string    `json:"path"`
-	PlaylistNames *[]string `json:"playlists"`
+	ID            playlistID `json:"id"`
+	Name          string     `json:"name"`
+	Path          string     `json:"path"`
+	PlaylistNames *[]string  `json:"playlists"`
 	Tracks        *[]Track
 }
 
 type Track struct {
-	ID   int64  `json:"id"`
-	Name string `json:"name"`
-	Path string `json:"path"`
+	ID   trackID `json:"id"`
+	Name string  `json:"name"`
+	Path string  `json:"path"`
 }
