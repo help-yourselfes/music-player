55d82eb22b5a4b35328b2898adb3526be29db51e
hys <helpyourselfes@gmail.com>
2026-05-16 12:22:04 +0300

    Implement AddPlaylist and GetPlaylistTracks to service

--- app/services/services.go
+++ app/services/services.go
@@ -31,3 +31,27 @@ func (s *AppService) AddTrack(ctx context.Context, reqTrack *models.Track) (int6
 
 	return id, nil
 }
+func (s *AppService) AddPlaylist(ctx context.Context, name string, path string) (int64, error) {
+	if name == "" {
+		return -1, errors.New("no name")
+	}
+
+	playlist := &models.Playlist{
+		Name: name,
+		Path: path,
+	}
+
+	id, err := s.playlists.Create(ctx, playlist)
+	if err != nil {
+		return -1, err
+	}
+
+	playlist.ID = id
+
+	return id, nil
+}
+
+func (s *AppService) GetPlaylistTracks(ctx context.Context, id models.PlaylistID) ([]*models.Track, error) {
+	list, err := s.playlists.ListTracks(ctx, id)
+	return list, err
+}
